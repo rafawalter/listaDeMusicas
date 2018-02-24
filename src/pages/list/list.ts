a26bbc413aa1392eb813f6c0385922f0818e7e63
Rafael Walter <rafawalter@gmail.com>
2018-02-24 09:10:27 -0300

    3 músicas para ajudar

--- src/pages/list/list.ts
+++ src/pages/list/list.ts
@@ -8,7 +8,7 @@ import { NavController, NavParams } from 'ionic-angular';
 export class ListPage {
   selectedItem: any;
   icons: string[];
-  items: Array<{moment: string, title: string, note: string, url: string, icon: string}>;
+  items: Array<{moment: string, title: string, tom: string, arranjo: string, url: string, icon: string}>;
 
   constructor(public navCtrl: NavController, public navParams: NavParams) {
     // If we navigated to this page, we will have an item available as a nav param
@@ -23,11 +23,41 @@ export class ListPage {
       this.items.push({
         moment: 'Momento ' + i,
         title: 'Música ' + i,
-        note: 'Arranjo #' + i,
+        tom: 'D',
+        arranjo: 'Arranjo #' + i,
         url: 'http://cifraclub.com.br',
         icon: this.icons[Math.floor(Math.random() * this.icons.length)]
       });
     }
+
+    this.items = [
+      {
+        moment: 'Entrada',
+        title: 'Chuva de graça',
+        tom: 'D',
+        arranjo: 'CifraClube',
+        url: 'https://www.cifraclub.com.br/eliana-ribeiro/chuva-de-graca/',
+        icon: this.iconeAleatorio()
+      },{
+        moment: 'Aclamação',
+        title: 'Tua palavra é luz para meu caminho (quero fazer valer)',
+        tom: 'E',
+        arranjo: 'CifraClube',
+        url: 'https://www.cifraclub.com.br/aline-barros/tua-palavra-e-luz-para-o-meu-caminho/',
+        icon: this.iconeAleatorio()
+      },{
+        moment: 'Comunhão',
+        title: 'De coração (tào distante e tão próximo',
+        tom: 'F',
+        arranjo: 'CifraClube',
+        url: 'https://www.cifraclub.com.br/vida-reluz/de-coracao/#key=1',
+        icon: this.iconeAleatorio()
+      }
+    ];
+  }
+
+  iconeAleatorio(): string {
+    return this.icons[Math.floor(Math.random() * this.icons.length)];
   }
 
   itemTapped(event, item) {
