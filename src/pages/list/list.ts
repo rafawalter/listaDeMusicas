2c96299be5d502c44f5cb0f71bc034b6573f7031
Rafael Walter <rafawalter@gmail.com>
2018-02-26 07:13:18 -0300

    Removendo lista.json

--- src/pages/list/list.ts
+++ src/pages/list/list.ts
@@ -12,33 +12,10 @@ export class ListPage {
   musicas: Musica[];
   selectedItem: any;
   icons: string[];
-  items: Array<{moment: string, title: string, tom: string, arranjo: string, url: string}>;
 
   constructor(public navCtrl: NavController, public navParams: NavParams, musicaService: MusicaService) {
     // If we navigated to this page, we will have an item available as a nav param
     this.selectedItem = navParams.get('item');
-
-    this.items = [
-      {
-        moment: 'Entrada',
-        title: 'Chuva de graça',
-        tom: 'D',
-        arranjo: 'CifraClube',
-        url: 'https://www.cifraclub.com.br/eliana-ribeiro/chuva-de-graca/',
-      },{
-        moment: 'Aclamação',
-        title: 'Tua palavra é luz para meu caminho (quero fazer valer)',
-        tom: 'E',
-        arranjo: 'CifraClube',
-        url: 'https://www.cifraclub.com.br/aline-barros/tua-palavra-e-luz-para-o-meu-caminho/',
-      },{
-        moment: 'Comunhão',
-        title: 'De coração (tão distante e tão próximo)',
-        tom: 'F',
-        arranjo: 'CifraClube',
-        url: 'https://www.cifraclub.com.br/vida-reluz/de-coracao/#key=1',
-      }
-    ];
 
     musicaService.list()
       .subscribe( 
